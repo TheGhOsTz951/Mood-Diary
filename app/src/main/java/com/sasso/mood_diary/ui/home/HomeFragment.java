53533558b28109a55d9bebf6bd04c95ba282cafb
Sasso <ricciuti.alessio@hotmail.com>
2020-10-04 11:29:50 +0200

    Bug solved

--- app/src/main/java/com/sasso/mood_diary/ui/home/HomeFragment.java
+++ app/src/main/java/com/sasso/mood_diary/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         return view;
     }
